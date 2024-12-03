4b385a406b8e0544251be6a74c1aead932e81d31
S0unn <green040711@gmail.com>
2024-12-03 18:08:51 +0900

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -16,7 +16,7 @@ const config = {
   url: 'https://team-unvs.github.io/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/unvs/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
